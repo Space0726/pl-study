9fe6a0226754a2912431f343e256fafe4c595898
space <space.sjwoo@gmail.com>
2020-10-03 23:47:58 +0900

    Adds Java class HashSet example

--- java/collections/HashSetExample.java
+++ java/collections/HashSetExample.java
@@ -2,6 +2,30 @@ import java.util.Set;
 import java.util.HashSet;
 import java.util.Iterator;
 
+class Animal {
+    String species;
+    String habitat;
+
+    Animal(String species, String habitat) {
+        this.species = species;
+        this.habitat = habitat;
+    }
+
+    public String toString() { return species + "-" + habitat; }
+
+    public int hashCode() { return (species + habitat).hashCode(); }
+
+    public boolean equals(Object obj) {
+        if (obj instanceof Animal) {
+            Animal temp = (Animal) obj;
+            return species.equals(temp.species) && habitat.equals(temp.habitat);
+        } else {
+            return false;
+        }
+    }
+
+}
+
 public class HashSetExample {
     public static void main(String[] args) {
         Set<String> hs1 = new HashSet<>();
@@ -25,6 +49,17 @@ public class HashSetExample {
         while (it.hasNext())
             System.out.println(it.next() + " ");
 
-        System.out.println("Size: " + hs2.size());
+        System.out.println("Size: " + hs2.size() + "\n");
+
+        // For class example
+        Set<Animal> hs = new HashSet<>();
+
+        hs.add(new Animal("Cat", "Land"));
+        hs.add(new Animal("Cat", "Land"));
+        hs.add(new Animal("Cat", "Land"));
+
+        System.out.println(hs.size());
+        for (Animal e: hs)
+            System.out.println(e);
     }
 }
